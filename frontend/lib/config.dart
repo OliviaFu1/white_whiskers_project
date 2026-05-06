a25326c26c3bd7db39b46e277ddb9d02dee82212
oliviafu <qf35@duke.edu>
2026-05-06 11:37:02 -0400

    changed base URL

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  // for Windows desktop / iOS simulator
-  static const String apiBaseUrl = "http://127.0.0.1:8000";
+  // for railway
+  static const String apiBaseUrl = "whitewhiskers-production.up.railway.app";
   // 10.0.2.2 routes to the host machine from the Android emulator
   // static const String apiBaseUrl = "http://10.0.2.2:8000";
 }
